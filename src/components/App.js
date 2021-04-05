eba981ff663faad45ab84d0b6795d6aaccbc3593
Joon Park <aisgandy@hotmail.com>
2021-04-05 15:46:55 +0900

    Change README.md

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,10 @@ class App extends React.Component {
     selectedVideo: null,
   };
 
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
